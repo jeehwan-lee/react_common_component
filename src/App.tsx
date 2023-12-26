c91af25179dc07ab7836a4d75e10db0381c7dba1
jeehwan <tjtojan@naver.com>
2023-12-26 11:18:13 +0900

    FEAT: 공통 Input 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import React from "react";
 import "./App.css";
 import Text from "./components/common/Text";
 import Button from "./components/common/Button";
+import Input from "./components/common/Input";
 
 function App() {
   return (
     <div className="App">
       <Button>테스트</Button>
+      <Input placeholder="로그인하세요" aria-invalid={false} />
     </div>
   );
 }
